5de487b637aaa074ed0f1c7c7fa3de653eb3cf29
cyphics <cyphics@protonmail.com>
2018-04-04 19:36:58 +0200

    add PhysicalUnit class

---
+++ src/physics/PhysicalUnit.java
@@ -0,0 +1,30 @@
+package physics;
+
+import util.BigNum;
+
+public class PhysicalUnit {
+	protected BigNum _num_value;
+	
+	public PhysicalUnit(BigNum num) {
+		
+	}
+	
+	public BigNum num() {
+		return _num_value;
+	}
+
+	public void set(PhysicalUnit val) {
+		_num_value = val.num();
+	}
+	public PhysicalUnit add(PhysicalUnit val) {
+		_num_value.add(val.num());
+		return this;
+	}
+	public PhysicalUnit sub(PhysicalUnit val) {
+		_num_value.sub(val.num());
+		return this;
+	}
+	public String toString() {
+		return _num_value.toString();
+	}
+}
